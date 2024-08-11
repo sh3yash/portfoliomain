09e35fe59c862988b677c2b1377c2d5459e215d5
YASH <sharmayash3343@gmail.com>
2024-08-11 23:19:37 +0530

    data changed

--- src/screens/projects/utils.tsx
+++ src/screens/projects/utils.tsx
@@ -4,10 +4,14 @@ import { SiTailwindcss, SiExpress} from 'react-icons/si';
 import { FaHtml5, FaCss3, FaReact, FaPython } from "react-icons/fa";
 import { TbBrandJavascript } from "react-icons/tb";
 
-import smartFoodLogo from 'assets/images/smart-food.png';
-
-import weatherAppLofo from 'assets/images/weather-app.png';
-import porfolioLogo from '../../../public/logo.svg';
+import shopify_clone from 'assets/images/shoping.jpeg';
+import food from 'assets/images/food.jpeg';
+import payment from 'assets/images/payment.png';
+import farmer from 'assets/images/farmer.jpg';
+import emotion from 'assets/images/emotion1.jpg';
+import student from 'assets/images/student.jpg';
+import shoping from 'assets/images/shoping.jpeg';
+import razor from 'assets/images/razorpay.jpeg';
 
 export interface Project {
   name: string;
@@ -29,7 +33,7 @@ export const customProjects: Project[] = [
     description: `Develop an integrated payments gateway solution that seamlessly integrates with a website or
               application, enabling secure and efficient online transactions, payment processing, and seamless
               financial transactions between customers and businesses.`,
-    img: porfolioLogo,
+    img: shopify_clone,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaReact key="react" color="#5ED4F4" className="text-[28px]" />,
@@ -45,7 +49,7 @@ export const customProjects: Project[] = [
     description: `🍽️ Discover our CSS & React-powered food ordering site. Enjoy a visually appealing interface 
               to explore diverse local menus. Customize and securely order your favorites for prompt delivery. Elevate your
               dining experience with convenience and quality.`,
-    img: porfolioLogo,
+    img: food,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaReact key="react" color="#5ED4F4" className="text-[28px]" />,
@@ -63,7 +67,7 @@ export const customProjects: Project[] = [
        multiple payment gateways, this solution provides all the tools you need. Simplify your
         financial operations and enhance customer experience with a modern, intuitive interface
          that ensures security and efficiency. 💳💼`,
-    img: weatherAppLofo,
+    img: razor,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaReact key="react" color="#5ED4F4" className="text-[28px]" />,
@@ -78,7 +82,7 @@ export const customProjects: Project[] = [
     description: `Design a college website to provide essential information, such as courses, faculty, admission details,
               events, and resources, ensuring a user-friendly experience for prospective students, current students,
               and staff members.`,
-    img: porfolioLogo,
+    img: student,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaHtml5 key="html" color='#ffA500' className="text-[28px]" />,
@@ -93,7 +97,7 @@ export const customProjects: Project[] = [
     description: `Empowering Farmers with Cutting-edge Tools! Explore seed prices,
      stay informed with real-time weather updates, unlock government schemes, and more.
       Cultivate success with smarter farming practices at your fingertips. 🌱🚜 `,
-    img: smartFoodLogo,
+    img: farmer,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaHtml5 key="html" color='#ffA500' className="text-[28px]" />,
@@ -104,12 +108,12 @@ export const customProjects: Project[] = [
   
 
   {
-    name: 'INTEGRATED PAYMENTS GATEWAY',
+    name: 'Integrated Payments Gateway',
     url: 'https://payment4integration.netlify.app/',
     description: `Develop an integrated payments gateway solution that seamlessly integrates with a website or
               application, enabling secure and efficient online transactions, payment processing, and seamless
               financial transactions between customers and businesses.`,
-    img: porfolioLogo,
+    img: payment,
     stack: [
       <FaCss3 key="css" color='#5ED4F4' className="text-[28px]" />,
       <FaHtml5 key="html" color='#ffA500' className="text-[28px]" />,
@@ -125,7 +129,7 @@ export const customProjects: Project[] = [
     description: `Design a Python-based face recognition attendance system for educational
             institutions and workplaces,enabling accurate and real-time tracking of individuals for
               efficient attendance management andrecord-keeping.`,
-    img: porfolioLogo,
+    img: emotion,
     stack: [
       <FaPython key="python" color="#5ED4F4" className="text-[28px]" />,
     ],
@@ -139,7 +143,7 @@ export const customProjects: Project[] = [
     description: `Develop an integrated payments gateway solution that seamlessly integrates with a website or
               application, enabling secure and efficient online transactions, payment processing, and seamless
               financial transactions between customers and businesses.`,
-    img: porfolioLogo,
+    img: shoping,
     stack: [
       <SiExpress key="expressjs" color="#5ED4F4" className="text-[28px]" />,
       <SiTailwindcss key="tailwind" color="#3FBDCC" className="text-[28px]" />,
